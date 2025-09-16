00b9560d7ee8719716575ad3f3999cf4c470dcd6
kaustubh <kaustubhtajne12@gmail.com>
2025-09-16 16:38:52 +0530

    added requestBody annotation

--- src/main/java/com/example/student_app/controller/StudentController.java
+++ src/main/java/com/example/student_app/controller/StudentController.java
@@ -5,10 +5,7 @@ import com.example.student_app.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,7 +27,7 @@ public class StudentController {
     }
 
     @PostMapping
-    public ResponseEntity<String> addStudent(Student student) {
+    public ResponseEntity<String> addStudent(@RequestBody Student student) {
         log.info("Student info - {}", student);
         studentService.createStudent(student);
         return new ResponseEntity<>("Student created Successfully", HttpStatus.CREATED);
